3849b47c382719f1b4e1e6d18c6212d721145d55
Hellen Silva <127620071+hellen-silvaa@users.noreply.github.com>
2024-06-12 17:38:41 -0300

    Add files via upload

---
+++ ws-python/operadores.py
@@ -0,0 +1,84 @@
+
+#------------ Operadores aritméticos ------------
+# + adição
+# - subtração
+# * multiplicação
+# / divisão
+# % resto da divisão ("mod")
+# ** exponenciação
+# // divisão inteira
+
+
+#*****************************************************
+
+#------------ Operadores comparativos ------------
+
+
+# < menor
+# > maior
+# <= menor ou igual
+# >= maior ou igual
+# == igual
+# != ou <> diferente
+
+#*****************************************************
+
+#------------ Operadores lógicos ------------
+
+# and e
+# or ou
+# not não
+
+#*****************************************************
+
+#------------ SAÍDA DE DADOS EM PYTHON ------------
+
+#print(" ")
+
+#Exemplo Python Resultado na tela
+
+#print("bom dia", end="") -> Bom diaBoa noite
+#print("boa noite", end="") ->
+
+#print("bom dia") -> Bom dia
+#print("boa noite") -> Boa noite
+
+#x: int; y: int
+#x = 10
+#y = 20
+#print(x) -> 10
+#print(y) -> 20
+
+#x: float
+#x = 2.3456
+#print("{:.2f}".format(x)) -> 2.35
+
+#idade: int
+#salario: float
+#nome: str
+#sexo: str
+#idade = 32
+#salario = 4560.9
+#nome = "Maria Silva"
+#sexo = "F"
+#print(f"A funcionaria {nome}, sexo {sexo}, ganha
+#{salario:.2f} e tem {idade} anos")
+#print("A funcionaria {:s}, sexo {:s}, ganha {:.2f}
+#e tem {:d} anos".format(nome, sexo, salario,
+#idade))
+
+#->A funcionaria Maria Silva, sexo F, ganha 4560.90 e tem 32 anos
+
+
+#*****************************************************
+
+#------------ Tipo Placeholder de formatação  ------------
+
+# int %d
+# float %f
+# str %s
+
+#*****************************************************
+#*****************************************************
+
+
---
+++ ws-python/programa1.py
@@ -0,0 +1,23 @@
+#declarando tipo de variavel (não é obrigatório o python sabe interpretar)
+#Python aceita atribuição diretamente na declaração da variável, e também aceita omissão do tipo.
+
+idade: int
+salario: float
+altura: float
+genero: str
+nome: str
+
+#declarando tipo de variavel
+
+
+idade = 21
+salario = 5800.5
+altura = 1.63
+genero = "F"
+nome = "Hellen Silva"
+
+print(f"IDADE = {idade}")
+print(f"SALARIO = {salario:.2f}")
+print(f"ALTURA = {altura:.2f}")
+print(f"GENERO = {genero}")
+print(f"NOME = {nome}")
